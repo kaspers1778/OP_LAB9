994de98f0cce0df05810b3b50142510b1fb5b0a4
Pete Balachin <pet.balachin1778@gmail.com>
2019-04-11 01:49:59 +0300

    Added algorythms but thrers is problem with spaces.

--- OP_LAB9/OP_LAB9/Form1.Designer.cs
+++ OP_LAB9/OP_LAB9/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_OpenFile = new System.Windows.Forms.Button();
+            this.dgv_Output = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Output)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_OpenFile
+            // 
+            this.btn_OpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btn_OpenFile.Name = "btn_OpenFile";
+            this.btn_OpenFile.Size = new System.Drawing.Size(99, 25);
+            this.btn_OpenFile.TabIndex = 0;
+            this.btn_OpenFile.Text = "Open file";
+            this.btn_OpenFile.UseVisualStyleBackColor = true;
+            this.btn_OpenFile.Click += new System.EventHandler(this.btn_OpenFile_Click);
+            // 
+            // dgv_Output
+            // 
+            this.dgv_Output.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Output.Location = new System.Drawing.Point(293, 12);
+            this.dgv_Output.Name = "dgv_Output";
+            this.dgv_Output.RowTemplate.Height = 24;
+            this.dgv_Output.Size = new System.Drawing.Size(479, 426);
+            this.dgv_Output.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 80);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 319);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgv_Output);
+            this.Controls.Add(this.btn_OpenFile);
+            this.Name = "Form1";
+            this.Text = "OP_LAB9";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Output)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_OpenFile;
+        private System.Windows.Forms.DataGridView dgv_Output;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- OP_LAB9/OP_LAB9/Form1.cs
+++ OP_LAB9/OP_LAB9/Form1.cs
@@ -7,14 +7,104 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace OP_LAB9
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            string Text;
+        }
+
+        private void btn_OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                Text = File.ReadAllText(file.FileName);
+            }
+            Text = Regex.Replace(Text, @"[^\w\s]", "");
+
+
+            char[] letters;
+            letters = Text.ToCharArray();
+            SortedList<char, int> sortedLetters = new SortedList<char, int>();
+            int tmp;
+
+            foreach(char letter in letters)
+            {               
+                if (!sortedLetters.ContainsKey(letter))
+                {
+                   
+                    sortedLetters.Add(letter, 0);
+                    
+                }
+                if (sortedLetters.ContainsKey(letter))
+                {
+                    tmp = sortedLetters[letter];
+                    sortedLetters.Remove(letter);
+                    sortedLetters.Add(letter, tmp + 1);
+                    tmp = 0;
+                }
+            }
+            
+            string[] words;
+            words = Text.Split();
+            SortedList<string, int> sortedWords = new SortedList<string, int>();
+
+            foreach (string word in words)
+            {
+                if (!sortedWords.ContainsKey(word))
+                {
+
+                    sortedWords.Add(word, 0);
+
+                }
+                if (sortedWords.ContainsKey(word))
+                {
+                    tmp = sortedWords[word];
+                    sortedWords.Remove(word);
+                    sortedWords.Add(word, tmp + 1);
+                    tmp = 0;
+                }
+            }
+
+            foreach(var el in sortedWords)
+            {
+                textBox1.Text += el.Key + " : " + el.Value + "\r\n";
+            }
+
+
+        }
+        
+        public class Letter
+        {
+            public char symbol;
+            public  int amount;
+
+            public Letter(char symbol, int amount)
+            {
+                this.symbol = symbol;
+                this.amount = amount;
+            }
+        }
+
+        public class Word
+        {
+            public string body;
+            public int amount;
+
+            public Word(string body, int amount)
+            {
+                this.body = body;
+                this.amount = amount;
+            }
         }
     }
 }
